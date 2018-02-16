37bc0a7cd58bf26a6383adc24c180d15c2721794
Apple <apple@Apples-iMac.local>
2018-02-16 14:33:56 +0200

    Bools, strings.

---
+++ bools_comparisons.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var amITheBestTeacher: Bool = true
+
+amITheBestTeacher = false
+
+if true == false || true == true {
+    print("WTFish")
+}
